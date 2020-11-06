3007de0e324fb6efcba79c91e3d6d5596b3c51ed
Jared Smith <jared817@hotmail.com>
2020-11-06 11:09:59 -0800

    Added Support for muliple decks

--- CardApiHelper.cs
+++ CardApiHelper.cs
@@ -11,18 +11,24 @@ namespace CardTests
 
         public static string url = "https://deckofcardsapi.com/api/deck/";
 
-        public static Deck GetNewDeck(bool withJokers)
+        public static Deck GetNewDeck(int numberOfDecks, bool withJokers)
         {
             var client = new RestClient(url);
             var request = new RestRequest("new", DataFormat.Json);
             var response = new RestResponse();
-            
+
+            if (numberOfDecks > 1)
+            {
+                request.AddParameter("deck_count", numberOfDecks);
+            }
+
             if (withJokers)
             {
-      
+
                 request.AddParameter("jokers_enabled", true);
                 response = (RestResponse)client.Post(request);
             }
+
             else
             {
                 response = (RestResponse)client.Get(request);
@@ -40,12 +46,13 @@ namespace CardTests
             return JsonConvert.DeserializeObject<Deck>(response.Content);
         }
 
-        public static bool VerifyDeckIsComplete(string deckId, int count, bool hasJokers)
+        public static bool VerifyDeckIsComplete(string deckId, int numberOfDecks, bool hasJokers)
         {
             var cardSuite = new List<string>() { "C", "D", "H", "S" };
             var cardValue = new List<string>() { "A", "2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K" };
             var fullCardSet = new List<string>();
             var hashCards = new HashSet<string>();
+            var deckSize = 52;
 
             // Create Full Card Set for verification
             foreach (var suite in cardSuite)
@@ -57,15 +64,17 @@ namespace CardTests
                 
             }
 
+
             // Add jokers if set
             if (hasJokers)
             {
                 fullCardSet.Add("X1");
                 fullCardSet.Add("X2");
+                deckSize += 2;
             }
 
             // Extract all cards from the deck
-            var cardDeck = DrawCards(deckId, count);
+            var cardDeck = DrawCards(deckId, numberOfDecks*deckSize);
             
             cardDeck.success.Should().BeTrue();
             cardDeck.deck_id.Should().Be(deckId);
@@ -79,12 +88,13 @@ namespace CardTests
                    return false;
                 }
 
-                // verify no duplicate cards in deck
+                // Get Unique Cards
                 hashCards.Add(card.code);
                 
             }
 
-            hashCards.Count.Should().Be(count);
+            // Verify base deck does not have duplicates
+            hashCards.Count.Should().Be(deckSize);
 
             return true;
         }
--- CardApiTests.cs
+++ CardApiTests.cs
@@ -11,41 +11,44 @@ namespace CardTests
         [Test]
         public void VerifyCreateNewDeckOfCards()
         {
-            var newDeck = CardApiHelper.GetNewDeck(false);
+            var numberOfDecks = 2;
+            var newDeck = CardApiHelper.GetNewDeck(numberOfDecks, withJokers:false);
 
             newDeck.success.Should().BeTrue();
             newDeck.deck_id.Should().NotBeNullOrEmpty();
             newDeck.shuffled.Should().BeFalse();
-            newDeck.remaining.Should().Be(52);
+            newDeck.remaining.Should().Be(numberOfDecks * 52);
 
-            CardApiHelper.VerifyDeckIsComplete(newDeck.deck_id, newDeck.remaining, false);
+            CardApiHelper.VerifyDeckIsComplete(newDeck.deck_id, numberOfDecks, hasJokers:false);
         }
 
         [Test]
         public void VerifyCreateNewDeckOfCardsWithJokers()
         {
-            var newDeck = CardApiHelper.GetNewDeck(true);
+            var numberOfDecks = 1;
+            var newDeck = CardApiHelper.GetNewDeck(numberOfDecks, withJokers:true);
 
             newDeck.success.Should().BeTrue();
             newDeck.deck_id.Should().NotBeNullOrEmpty();
             newDeck.shuffled.Should().BeFalse();
-            
-            // Add Jokers functionality does not seem to be working on the API
-            //newDeck.remaining.Should().Be(54);
-            newDeck.remaining.Should().Be(52);
 
-            CardApiHelper.VerifyDeckIsComplete(newDeck.deck_id, newDeck.remaining, true);
+            // Add Jokers functionality does not seem to be working on the API
+            //newDeck.remaining.Should().Be(numberOfDecks * 54);
+            newDeck.remaining.Should().Be(numberOfDecks * 52);
+
+            CardApiHelper.VerifyDeckIsComplete(newDeck.deck_id, numberOfDecks, hasJokers:true);
         }
 
         [Test]
         public void VerifyDrawCardsFromDeck()
         {
-            var newDeck = CardApiHelper.GetNewDeck(false);
+            var numberOfDecks = 1;
+            var newDeck = CardApiHelper.GetNewDeck(numberOfDecks, withJokers:false);
 
             newDeck.success.Should().BeTrue();
             newDeck.deck_id.Should().NotBeNullOrEmpty();
             newDeck.shuffled.Should().BeFalse();
-            newDeck.remaining.Should().Be(52);
+            newDeck.remaining.Should().Be(numberOfDecks * 52);
 
             var rnd = new Random();
             var randomCards = rnd.Next(1, newDeck.remaining);
